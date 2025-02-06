ed1479914f48838d9438b3df26a2cd574d0f3dff
Daniel Matthews <d.matthews@ucl.ac.uk>
2025-02-06 16:01:12 +0000

    Make networks local a list

--- modules/k3s-cluster/main.tf
+++ modules/k3s-cluster/main.tf
@@ -2,34 +2,29 @@ module "k3s_server_vm" {
   count  = local.vm_count
   source = "../virtual-machine"
 
-  appstream_repo_url = var.appstream_repo_url
-  additional_disks   = var.additional_disks
-  baseos_repo_url    = var.baseos_repo_url
-  cpu                = var.cpu
-  efi_boot           = var.efi_boot
-  memory             = var.memory
-  name               = local.vm_names[count.index]
-  namespace          = var.namespace
-  primary_interface  = var.primary_interface
-  root_disk_size     = var.root_disk_size
-  run_strategy       = var.run_strategy
-  ssh_public_key     = tls_private_key.ssh.public_key_openssh
-  ssh_private_key    = tls_private_key.ssh.private_key_openssh
+  additional_disks = var.additional_disks
+  cpu              = var.cpu
+  efi_boot         = var.efi_boot
+  memory           = var.memory
+  name             = local.vm_names[count.index]
+  namespace        = var.namespace
+  networks         = local.networks[count.index]
+  network_data = templatefile("${path.module}/templates/network_data.yaml.tftpl", {
+    networks = local.networks[count.index]
+  })
+  primary_interface = var.primary_interface
+  root_disk_size    = var.root_disk_size
+  run_strategy      = var.run_strategy
+  ssh_private_key   = tls_private_key.ssh.private_key_openssh
+  user_data = templatefile("${path.module}/templates/user_data.yaml.tftpl", {
+    additional_disks   = try(var.additional_disks, "")
+    appstream_repo_url = var.appstream_repo_url
+    baseos_repo_url    = var.baseos_repo_url
+    ssh_public_key     = tls_private_key.ssh.public_key_openssh
+  })
   vm_image           = var.vm_image
   vm_image_namespace = var.vm_image_namespace
   vm_username        = var.vm_username
-
-  networks = {
-    for key, value in var.networks : key =>
-    {
-      cidr    = value.cidr
-      dns     = value.dns
-      gateway = value.gateway
-      iface   = key
-      ip      = value.ips[count.index]
-      network = value.network
-    }
-  }
 }
 
 module "install_k3s" {
