993ef2adf2e725af65330aeaf6331f0ad60810bd
Daniel Matthews <d.matthews@ucl.ac.uk>
2025-05-09 09:55:12 +0100

    Remove ssh key generation from kairos-k3s-cluster module

--- modules/kairos-k3s-cluster/ssh_key.tf
+++
@@ -1,8 +0,0 @@
-resource "tls_private_key" "ssh" {
-  algorithm = "ED25519"
-}
-
-resource "local_sensitive_file" "ssh_key" {
-  filename = "${path.module}/ssh-private-key"
-  content  = tls_private_key.ssh.private_key_openssh
-}
